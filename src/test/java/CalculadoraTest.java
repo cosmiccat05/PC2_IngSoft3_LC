9ba02a26058af66f2a6b5dec9b35a76dde74a71f
cosmiccat05 <lucerito14_10@hotmail.com>
2025-11-21 13:42:29 -0500

    PC2-Software3

---
+++ src/test/java/CalculadoraTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CalculadoraTest {
+    @Test
+    void testSuma() {
+        assertEquals(5, Calculadora.sumar(2, 3));
+    }
+}
+
